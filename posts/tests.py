4a8e5ded9fcb1f11bdf40c2ffdd2fb686c99d90a
Gaurang Pateriya <gap6033@gmail.com>
2023-08-26 17:01:38 +0530

    Added tests for posts app

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
+from .models import Post
+from django.urls import reverse
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self) -> None:
+        Post.objects.create(text='A test post')
+
+    def test_post_text_content(self):
+        post = Post.objects.get(id=1)
+        returned_text = post.text[:50]
+        self.assertEqual(returned_text, 'A test post')
+
+class HomePageViewTest(TestCase):
+    def test_home_page_access(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_access_via_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_right_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
